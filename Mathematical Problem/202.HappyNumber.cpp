bfa3c0766bf54c05d1c85a4a62ef429dc4c54828
wolfgunblood <jalajdorai100@gmail.com>
2024-02-20 00:10:53 +0530

    202. Happy Number

---
+++ Mathematical Problem/202.HappyNumber.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int sumOfSquares(int n){
+        int sum =0 ;
+        while(n)
+        {
+            int d = n%10;
+            sum = sum + d*d;
+            n=n/10;
+        }
+
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        
+        int slow = n;
+        int fast = sumOfSquares(n);
+
+        if(slow == 1 || fast == 1) return true;
+        
+        while(slow != fast){
+
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+
+            if(slow == 1 || fast == 1) return true;
+        }
+
+        return false;
+    }
+};
