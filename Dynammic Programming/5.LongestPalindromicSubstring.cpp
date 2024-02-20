9cba5616713ca5b715ae7d91d62d4c3d74c5c257
wolfgunblood <jalajdorai100@gmail.com>
2024-02-20 20:36:31 +0530

    647. Palindromic Substrings

--- Dynammic Programming/5.LongestPalindromicSubstring.cpp
+++ Dynammic Programming/5.LongestPalindromicSubstring.cpp
@@ -5,7 +5,7 @@ public:
         int maxStart = 0;
         int maxLength = 1;
 
-        for(int i =0;i<s.size()-1;i++)
+        for(int i =0;i<s.size()-1;i++) //for(int i =0;i<s.size()-1;i++)
         {
             middleInOut(s,i,i,maxStart,maxLength);
             middleInOut(s,i,i+1,maxStart,maxLength);
