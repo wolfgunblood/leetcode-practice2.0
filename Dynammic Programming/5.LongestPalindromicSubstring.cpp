460fa26a8b96b0175ecbfc0835721b44d9fbbaf3
wolfgunblood <jalajdorai100@gmail.com>
2024-02-20 20:19:54 +0530

    5. Longest Palindromic Substring

---
+++ Dynammic Programming/5.LongestPalindromicSubstring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        int maxStart = 0;
+        int maxLength = 1;
+
+        for(int i =0;i<s.size()-1;i++)
+        {
+            middleInOut(s,i,i,maxStart,maxLength);
+            middleInOut(s,i,i+1,maxStart,maxLength);
+        }
+         return s.substr(maxStart,maxLength);
+        }
+    
+
+    void middleInOut(string s,int i,int j,int& maxStart,int& maxLength){
+
+        while(i>=0 && j<=s.size() - 1 && s[i] == s[j]){
+
+            i--;
+            j++;
+        }
+
+        if(j-i-1 >maxLength){
+            maxStart =i+1;
+            maxLength =j-i-1;
+        }
+    }
+};
