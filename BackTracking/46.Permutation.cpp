9f38f06bf3e82c1b9c8cb68ff5dd46e3ad6d8827
wolfgunblood <jalajdorai100@gmail.com>
2024-02-23 13:04:28 +0530

    46. Permutations

---
+++ BackTracking/46.Permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>> result;
+        dfs(nums,0,result);
+
+        return result;
+    }
+
+    void dfs(vector<int>& nums,int start,vector<vector<int>>& result){
+        
+      
+        
+        if(start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for(int i =start;i<nums.size();i++){
+            swap(nums[start],nums[i]);
+            dfs(nums,start+1,result);
+            swap(nums[i],nums[start]);
+        }
+    }
+};
